eaddd4d8edde3f91c8dcc4a6c38c052b9f6b95b6
adam flood <adam flood>
2016-12-14 23:15:02 +0000

    Initial commit

---
+++ app/src/main/java/com/example/adamf/myfitnessapplicationgoogleapi/NewMain2.java
@@ -0,0 +1,15 @@
+package com.example.adamf.myfitnessapplicationgoogleapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMain2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_main2);
+
+        
+    }
+}
