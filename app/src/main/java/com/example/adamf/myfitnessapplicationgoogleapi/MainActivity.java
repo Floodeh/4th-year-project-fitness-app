7bba2377bb4b587bee062b51258fbd3f5ac7bbeb
Floodeh <adamflood888@hotmail.com>
2017-02-16 15:39:38 +0000

    View steps taken each day for the last seven days

--- app/src/main/java/com/example/adamf/myfitnessapplicationgoogleapi/MainActivity.java
+++ app/src/main/java/com/example/adamf/myfitnessapplicationgoogleapi/MainActivity.java
@@ -59,7 +59,10 @@ GoogleApiClient.OnConnectionFailedListener,
 View.OnClickListener{
 
     private Button mButtonViewWeek;
-
+    private Button mButtonViewToday;
+    private Button mButtonAddSteps;
+    private Button mButtonUpdateSteps;
+    private Button mButtonDeleteSteps;
     private Button mCancelSubscriptionsBtn;
     private Button mShowSubscriptionsBtn;
 
@@ -103,7 +106,19 @@ View.OnClickListener{
     private void initViews() {
         mCancelSubscriptionsBtn = (Button) findViewById(R.id.btn_cancel_subscriptions);
         mShowSubscriptionsBtn = (Button) findViewById(R.id.btn_show_subscriptions);
+        mButtonViewWeek = (Button) findViewById(R.id.btn_view_week);
+        mButtonViewToday = (Button) findViewById(R.id.btn_view_today);
+        mButtonAddSteps = (Button) findViewById(R.id.btn_add_steps);
+        mButtonUpdateSteps = (Button) findViewById(R.id.btn_update_steps);
+        mButtonDeleteSteps = (Button) findViewById(R.id.btn_delete_steps);
 
+
+
+        mButtonViewWeek.setOnClickListener(this);
+        mButtonViewToday.setOnClickListener(this);
+        mButtonAddSteps.setOnClickListener(this);
+        mButtonUpdateSteps.setOnClickListener(this);
+        mButtonDeleteSteps.setOnClickListener(this);
         mCancelSubscriptionsBtn.setOnClickListener(this);
         mShowSubscriptionsBtn.setOnClickListener(this);
     }
@@ -228,6 +243,26 @@ View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
+            case R.id.btn_view_week: {
+                new ViewWeekStepCountTask().execute();
+                break;
+            }
+//            case R.id.btn_view_today: {
+//                new ViewTodaysStepCountTask().execute();
+//                break;
+//            }
+//            case R.id.btn_add_steps: {
+//                new AddStepsToGoogleFitTask().execute();
+//                break;
+//            }
+//            case R.id.btn_update_steps: {
+//                new UpdateStepsOnGoogleFitTask().execute();
+//                break;
+//            }
+//            case R.id.btn_delete_steps: {
+//                new DeleteYesterdaysStepsTask().execute();
+//                break;
+//            }
             case R.id.btn_cancel_subscriptions: {
                 cancelSubscriptions();
                 break;
@@ -251,6 +286,7 @@ View.OnClickListener{
 
     @Override
     public void onConnectionSuspended(int i) {
+        Log.e("HistoryAPI", "onConnectionSuspended");
 
     }
 
@@ -297,8 +333,72 @@ View.OnClickListener{
         }
     }
 
+    private class ViewWeekStepCountTask extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... params) {
+            displayLastWeeksData();
+            return null;
+        }
+    }
+
+    private void displayLastWeeksData() {
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        java.text.DateFormat dateFormat = DateFormat.getDateInstance();
+        Log.e("History", "Range Start: " + dateFormat.format(startTime));
+        Log.e("History", "Range End: " + dateFormat.format(endTime));
+
+        //Check how many steps were walked and recorded in the last 7 days
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        DataReadResult dataReadResult = Fitness.HistoryApi.readData(mApiClient, readRequest).await(1, TimeUnit.MINUTES);
+
+        //Used for aggregated data
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.e("History", "Number of buckets: " + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    showDataSet(dataSet);
+                }
+            }
+        }
+        //Used for non-aggregated data
+        else if (dataReadResult.getDataSets().size() > 0) {
+            Log.e("History", "Number of returned DataSets: " + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                showDataSet(dataSet);
+            }
+        }
+    }
+
+    private void showDataSet(DataSet dataSet) {
+        Log.e("History", "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        DateFormat timeFormat = DateFormat.getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.e("History", "Data point:");
+            Log.e("History", "\tType: " + dp.getDataType().getName());
+            Log.e("History", "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.e("History", "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            for(Field field : dp.getDataType().getFields()) {
+                Log.e("History", "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
 
 
+//total steps taken
     private class FetchStepsAsync extends AsyncTask<Object, Object, Long> {
         protected Long doInBackground(Object... params) {
             long total = 0;
