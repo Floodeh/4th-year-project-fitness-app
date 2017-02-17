bace7372fe4a414e2540eeb04bebc88a5f45068f
Floodeh <adamflood888@hotmail.com>
2017-02-17 16:18:34 +0000

    Pie Chart graph added(HARDCODED VALUES)

--- app/src/main/java/com/example/adamf/myfitnessapplicationgoogleapi/MainActivity.java
+++ app/src/main/java/com/example/adamf/myfitnessapplicationgoogleapi/MainActivity.java
@@ -133,33 +133,33 @@ View.OnClickListener{
         mCancelSubscriptionsBtn.setOnClickListener(this);
         mShowSubscriptionsBtn.setOnClickListener(this);
 
-//        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+
+        mPieChart.addPieSlice(new PieModel("Steps", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Goal", 25, Color.parseColor("#56B7F1")));
+
+        mPieChart.startAnimation();
+
+//        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
 //
-//        mPieChart.addPieSlice(new PieModel("Steps", 15, Color.parseColor("#FE6DA8")));
-//        mPieChart.addPieSlice(new PieModel("Goal", 25, Color.parseColor("#56B7F1")));
+//        ValueLineSeries series = new ValueLineSeries();
+//        series.setColor(0xFF56B7F1);
 //
-//        mPieChart.startAnimation();
-
-        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
-
-        ValueLineSeries series = new ValueLineSeries();
-        series.setColor(0xFF56B7F1);
-
-        series.addPoint(new ValueLinePoint("Jan", 2.4f));
-        series.addPoint(new ValueLinePoint("Feb", 3.4f));
-        series.addPoint(new ValueLinePoint("Mar", .4f));
-        series.addPoint(new ValueLinePoint("Apr", 1.2f));
-        series.addPoint(new ValueLinePoint("Mai", 2.6f));
-        series.addPoint(new ValueLinePoint("Jun", 1.0f));
-        series.addPoint(new ValueLinePoint("Jul", 3.5f));
-        series.addPoint(new ValueLinePoint("Aug", 2.4f));
-        series.addPoint(new ValueLinePoint("Sep", 2.4f));
-        series.addPoint(new ValueLinePoint("Oct", 3.4f));
-        series.addPoint(new ValueLinePoint("Nov", .4f));
-        series.addPoint(new ValueLinePoint("Dec", 1.3f));
-
-        mCubicValueLineChart.addSeries(series);
-        mCubicValueLineChart.startAnimation();
+//        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+//        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+//        series.addPoint(new ValueLinePoint("Mar", .4f));
+//        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+//        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+//        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+//        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+//        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+//        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+//        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+//        series.addPoint(new ValueLinePoint("Nov", .4f));
+//        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+//
+//        mCubicValueLineChart.addSeries(series);
+//        mCubicValueLineChart.startAnimation();
     }
 
     private void initCallbacks() {
