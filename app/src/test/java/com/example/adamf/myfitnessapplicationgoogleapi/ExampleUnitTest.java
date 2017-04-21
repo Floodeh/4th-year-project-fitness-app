9aa7cf517e74359e34e9e4440cb6b42df170c4e8
Floodeh <adamflood888@hotmail.com>
2017-04-21 14:56:40 +0100

    PROJECT FINAL BEFORE CLEANUP

--- app/src/test/java/com/example/adamf/myfitnessapplicationgoogleapi/ExampleUnitTest.java
+++ app/src/test/java/com/example/adamf/myfitnessapplicationgoogleapi/ExampleUnitTest.java
@@ -36,10 +36,31 @@ public class ExampleUnitTest {
 
         float expected = 50;
 
+        //The user is not walking therefore the confidence found should not be equal to the expected
+        //value. The test should pass
         assertNotEquals("Check if walking time is not equal to actual:", expected, actual, 0.001);
-        //assertEquals("Confidence of walking time matching failed!", expected, actual, 0.001);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //Working test
     //RESULT PASSED
@@ -50,14 +71,18 @@ public class ExampleUnitTest {
 
         float expected = 0;
 
+        //User is not driving therefor the the expected value should be 0
+        //the assertEquals checks if the expected(0) is the same as the actual (0)
         assertEquals("Confidence of driving matching Success!", expected, actual, 0.001);
 
     }
 
+
+
     //Working test
     //RESULT FAIL
     @Test
-    public void checkStillConfidence() {
+    public void checkStillConfidence() throws Exception {
 
         float actual = MainActivity.stillTime;
 
@@ -66,6 +91,49 @@ public class ExampleUnitTest {
         //user is standing still therefore the value should be 100 as the api
         //recognises that the user is standing still.
         assertNotEquals("Confidence of still matching Failed!", expected, actual, 0.001);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Working test
+    //RESULT PASS
+    @Test
+    public void checkStillConfidenceEquals() throws Exception {
+
+        float actual = MainActivity.stillTime;
+
+        float expected = 100;
+
+        //user is standing still therefore the value should be 100 as the api
+        //recognises that the user is standing still.
+        //checking to see if the expected 100, matches the actual 100
+        assertEquals("Confidence of still matching Correct!", expected, actual, 0.001);
 
     }
 }
